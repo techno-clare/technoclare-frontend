722772b310754a5fa319b5ced8d34333fc24f2c7
vivekranjansahoo <vivekranjansahoo81@gmail.com>
2024-09-25 00:20:49 +0530

    new start

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-sm font-bold underline ">Hello world!</h1>
+      <h1 className="text-sm font-bold underline ">New start!</h1>
     </>
   );
 }
