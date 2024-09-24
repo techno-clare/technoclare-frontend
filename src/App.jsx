03724b559106033ec55185a2e003e43a50d593d0
vivekranjansahoo <vivekranjansahoo81@gmail.com>
2024-09-24 23:44:11 +0530

    🎉 init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-sm font-bold underline ">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
